633b5fb8f74323d034751aa274ecb3c2bf4dd03b
Ryan Saunders <ryan.saunders89@gmx.com>
2019-11-22 10:23:41 +0000

    Uploaded code for Bookmarklet

---
+++ sfAuto Bookmarklet.js
@@ -0,0 +1,6 @@
+javascript(function(){
+	var%20script=document.createElement('script');
+	script.type='text/javascript';
+	script.src='https://raw.githubusercontent.com/IDemixI/sfAutoRefresh/master/sfAuto.js';
+	document.getElementsByTagName('head')[0].appendChild(script);
+})();
