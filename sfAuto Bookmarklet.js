7d4f45c621e23e140e5d85da8ea95a8c853a2775
Ryan Saunders <ryan.saunders89@gmx.com>
2019-11-22 10:35:18 +0000

    Fixed URL for main script.

--- sfAuto Bookmarklet.js
+++ sfAuto Bookmarklet.js
@@ -1,6 +1,6 @@
-javascript(function(){
+javascript:(function(){
 	var%20script=document.createElement('script');
 	script.type='text/javascript';
-	script.src='https://raw.githubusercontent.com/IDemixI/sfAutoRefresh/master/sfAuto.js';
+	script.src='https://cdn.jsdelivr.net/gh/IDemixI/sfAutoRefresh@master/sfAuto.js';
 	document.getElementsByTagName('head')[0].appendChild(script);
 })();
