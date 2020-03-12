4cb6269444260b4c22300f25a0ce5f954250ced2
Ryan Saunders <ryan.saunders89@gmx.com>
2020-03-12 17:00:12 +0000

    Update GM sfAutoCC Caller.js

--- GM sfAutoCC Caller.js
+++ GM sfAutoCC Caller.js
@@ -3,7 +3,7 @@
 // @version  1.0
 // @grant    none
 // @match https://eu12.salesforce.com/500/e?retURL=%2F500%2Fo&RecordType=012200000000Ovb&ent=Case&FME*
-// @match https://tasks.office.com/1spatial.com/en-GB/Home/Planner#/plantaskboard?groupId=aa41d79b-7cbb-4b64-b094-4ea3b6a3091a&planId=l2pbPkT0JkShbOl7FHfvFpYAAFjs
+// @match https://tasks.office.com/*
 // ==/UserScript==
 
 function loadsfAutoCC(){
